2f6451b9f5971efd958a887f8bdddd1ada853e12
rhs <ar.ryuhs@gmail.com>
2019-04-07 23:50:22 +0900

    spring boot init - toy torrent - hello world

---
+++ src/main/java/toy/torrent/system/controller/ToySystemController.java
@@ -0,0 +1,15 @@
+package toy.torrent.system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("toysystem/v1")
+@RestController
+public class ToySystemController {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
