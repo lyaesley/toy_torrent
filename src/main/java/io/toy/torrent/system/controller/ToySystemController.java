0b29733d220ac80cb3ec62ccd4af13e2633acc99
rhs <ar.ryuhs@gmail.com>
2019-04-08 23:02:00 +0900

    project 경로 수정

--- src/main/java/io/toy/torrent/system/controller/ToySystemController.java
+++ src/main/java/io/toy/torrent/system/controller/ToySystemController.java
@@ -1,4 +1,4 @@
-package toy.torrent.system.controller;
+package io.toy.torrent.system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
